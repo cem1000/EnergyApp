e09295e9211c55cd721e363a84e231cdb357afe7
cem1000 <55397332+cem1000@users.noreply.github.com>
2024-01-28 15:25:39 +0000

    Update CleanDataScript (1).py

--- CleanDataScript (1).py
+++ CleanDataScript (1).py
@@ -1,9 +1,3 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import warnings
-
-
 # Let's create a description of what the scopes are. Found online. 
 def label_scope(value):
     if value == 1:
@@ -17,9 +11,9 @@ def label_scope(value):
 
 def set_renewable_flag(row):
     if row == 'y':
-        return True
+        return 'Renewable Source'
     else:
-        return False
+        return 'Non-renewable Source'
 
 # Function to call data
 def load_data():
@@ -32,19 +26,33 @@ def load_data():
     df['renewable_flag'] = df['renewable'].apply(set_renewable_flag)
 
     # making sure end_date correct format
-    df['end_date'] = pd.to_datetime(df['end_date'])
+    df['end_date'] = pd.to_datetime(df['end_date'], dayfirst=True).dt.strftime('%Y-%m')
+
+    df['month'] = pd.to_datetime(df['end_date']).dt.strftime('%Y-%m')  # Ensure month is a string 'YYYY-MM'
 
     # Ensuring subset cols
-    cleaned_data = df[['end_date', 'division', 'scope_description', 'renewable_flag', 'data_type', 'utility', 'scope_text', 'kwh', 'kgco2e']]
+    cleaned_data = df[['month', 'division', 'scope_description', 'renewable_flag', 'data_type', 'utility', 'scope_text', 'kwh', 'kgco2e']]
 
     # Grouping the data agg by the two metrics
     grouped_data = cleaned_data.groupby(
-        ['end_date', 'division', 'scope_description', 'renewable_flag', 'data_type', 'utility', 'scope_text']
+        ['month', 'division', 'scope_description', 'renewable_flag', 'data_type', 'utility', 'scope_text']
     ).agg({
         'kwh': 'sum',
         'kgco2e': 'sum'
     }).reset_index()
 
+    # Replace NaN values with zeros in the 'kwh' and 'kgco2e' columns as it creates issues down the line
+    grouped_data['kwh'].fillna(0, inplace=True)
+    grouped_data['kgco2e'].fillna(0, inplace=True)    
+    
+    # calling it df
     df = grouped_data
     
+    # renaming to df
+    df = df.rename(columns={'data_type': 'utility_type'})
+    
+    # fixing month here. 
+    df['month'] = pd.to_datetime(df['month']).dt.to_period('M')
+    df = df[df['month'] < "2024-01"]
     return df
+
